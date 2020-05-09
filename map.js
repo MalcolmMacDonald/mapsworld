50ad03f3dc6a109a1034e1a0cfef0e12ed3cfdae
DESKTOP-1GU391B\Malcolm MacDonald <mac97@shaw.ca>
2020-05-09 15:27:39 -0700

    added map

---
+++ map.js
@@ -0,0 +1,12 @@
+
+
+function initialize() {
+    var fenway = { lat: 42.345573, lng: -71.098326 };
+    var panorama = new google.maps.StreetViewPanorama(
+        document.getElementById('pano'), {
+        position: fenway,
+        disableDefaultUI: true
+    });
+    map.setStreetView(panorama);
+
+}
