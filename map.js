e540a9832aa2f1f632593f51f5e51bf34ca391dc
DESKTOP-1GU391B\Malcolm MacDonald <mac97@shaw.ca>
2020-05-14 00:36:48 -0700

    swapped heading and pitch

--- map.js
+++ map.js
@@ -1,6 +1,6 @@
 function initialize() {
     var fenway = { lat: 39.8341082, lng: -104.6762833 };
-    var direction = { heading: 43.06, pitch: 150.44 };
+    var direction = { heading: 150.44, pitch: 43.06 };
     var panorama = new google.maps.StreetViewPanorama(
         document.getElementById('pano'), {
         position: fenway,
