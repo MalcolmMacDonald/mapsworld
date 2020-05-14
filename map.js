e00feac64258133e392b69b8145dbf27ada1808d
DESKTOP-1GU391B\Malcolm MacDonald <mac97@shaw.ca>
2020-05-13 23:26:49 -0700

    changed position, removed control

--- map.js
+++ map.js
@@ -1,9 +1,11 @@
 function initialize() {
-    var fenway = { lat: 52.3744677, lng: 4.9004073 };
+    var fenway = { lat: 49.269459, lng: -123.181103 };
     var panorama = new google.maps.StreetViewPanorama(
         document.getElementById('pano'), {
         position: fenway,
-        disableDefaultUI: true
+        disableDefaultUI: true,
+        clickToGo: false,
+        linksControl: false
     });
     map.setStreetView(panorama);
 }
