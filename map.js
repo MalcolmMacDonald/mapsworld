80c986c004bc37a18f7a8243473cf0fdab32901f
DESKTOP-1GU391B\Malcolm MacDonald <mac97@shaw.ca>
2020-05-13 23:51:41 -0700

    updated new street view position

--- map.js
+++ map.js
@@ -1,5 +1,5 @@
 function initialize() {
-    var fenway = { lat: 49.0292818, lng: -122.8015831 };
+    var fenway = { lat: 43.6537984, lng: -79.4003736 };
     var panorama = new google.maps.StreetViewPanorama(
         document.getElementById('pano'), {
         position: fenway,
