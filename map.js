4896406ea5711d0018b174b19da2d20b6e5a223e
DESKTOP-1GU391B\Malcolm MacDonald <mac97@shaw.ca>
2020-05-14 00:32:15 -0700

    updated location

--- map.js
+++ map.js
@@ -1,8 +1,11 @@
 function initialize() {
     var fenway = { lat: 43.6537984, lng: -79.4003736 };
+    var direction = { heading: 46.2, pitch: 151.69 };
     var panorama = new google.maps.StreetViewPanorama(
         document.getElementById('pano'), {
         position: fenway,
+        pov: direction,
+        zoom: 29,
         disableDefaultUI: true,
         clickToGo: false,
         linksControl: false
